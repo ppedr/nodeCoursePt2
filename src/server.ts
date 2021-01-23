e09494d85d87639ba6c99b5a3a6e75e1df325ea9
ppedr <ppedrofreitas@outlook.com.br>
2021-01-23 16:10:05 -0300

    first commit: TS app

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(4000, () => {
+    console.log('Server started! ✔');
+});
+
