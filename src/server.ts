e281492ea33209f8e1c771c196eb62d2eb955986
ppedr <ppedrofreitas@outlook.com.br>
2021-01-24 16:42:58 -0300

    4th commit: seeds added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(4000, () => {
     console.log('Server started! ✔');
